462d11d43e8ca3963fc5a73b5d9f34986adaaa43
soumikpatra1993 <tutul.smk@gmail.com>
2017-01-11 02:36:12 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,9 @@
+            var name= "SOUMIK PATRA";
+			var age=23;
+			var DOB="29th DECEMBER 1993";
+
+			var Place="RIHAND NAGAR";
+		console.log(name);
+		console.log(age);
+		console.log(DOB);
+		console.log(Place);
