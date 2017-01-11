7358b6653502048239bb6045ebc11abe751654b1
soumikpatra1993 <tutul.smk@gmail.com>
2017-01-11 14:40:47 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,17 @@
+var employee = {
+  name:"SOUMIK PATRA",
+  age: 23,
+  salary: 25000,
+  address:{
+  	city: "Bangalore",
+  	state:"Karnataka",
+  	pin:560078
+  }
+
+};
+console.log(employee.name);
+console.log(employee.age);		
+console.log(employee.salary);
+console.log(employee.address.city);
+console.log(employee.address.state);
+console.log(employee.address.pin);
